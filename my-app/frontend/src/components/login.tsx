3b0d950075d72c4f14620ade6c66db6ae8e581e1
myllmat22 <matiasmyllymaki05@gmail.com>
2025-10-25 21:08:26 +0300

    login form started, figure out what to do after login

--- my-app/frontend/src/components/login.tsx
+++ my-app/frontend/src/components/login.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8081/login')
@@ -10,16 +12,34 @@ function UserList() {
       .then(data => setUsers(data))
       .catch(err => console.error(err));
   }, []);
-  console.log(users);
+
+  const loginHandler = () => {
+    users.map((user: { id: number; name: string, password: string }) => {
+      if (user.name === username && user.password === password) {
+        console.log("joojoo");
+      }
+      else {
+        console.log("noo");
+      }
+    });
+  }
 
   return (
-    <div>
-      <h2>User List</h2>
-      <ul>
-        {users.map((user: { id: number; name: string }) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+    <div className="login-parent">
+      <div className="login-child">
+        <h2>User List</h2>
+        <ul>
+          {users.map((user: { id: number; name: string, password: string }) => (
+            <li key={user.id}>{user.name} - {user.password}</li>
+          ))}
+        </ul>
+        <form className="login-form">
+          <input onChange={e => setUsername(e.target.value)} type="text" placeholder="Name" value={username} required />
+          <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" value={password} required />
+          <button type="button" onClick={loginHandler}>Login</button>
+        </form>
+      </div>
+      <hr />
     </div>
   );
 }
